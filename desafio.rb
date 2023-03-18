7c0ca7acd6582f7e0ed5e499b40f6c11c438dc07
israh-lopes <israel.lopes@hotmail.com>
2023-03-18 13:30:03 -0300

    desafio curso

---
+++ desafio.rb
@@ -0,0 +1,7 @@
+#Usuario digitar o nome
+print "Olá, por favor digite seu nome completo: "
+nomecompleto =gets.chomp
+print "Obrigado pelas informações, agora me diga qual sua idade: "
+idade = gets.chomp
+puts "Seu nome é #{nomecompleto}, e sua idade é #{idade} anos"
+#consegui
